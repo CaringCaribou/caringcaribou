d006454a055790801fd84718a91d3a26a4c1acb4
Craig Smith <agent.craig@gmail.com>
2015-06-30 09:31:42 -0700

    Added more NRC error code definitions

--- tool/modules/dcm.py
+++ tool/modules/dcm.py
@@ -39,12 +39,23 @@ NRC = {
     0x11: 'serviceNotSupported',
     0x12: 'sub-functionNotSupported',
     0x13: 'incorrectMessageLengthOrInvalidFormat',
+    0x14: 'responseTooBig',
     0x21: 'busyRepeatRequest',
     0x22: 'conditionsNotCorrect',
+    0x24: 'requestSequenceError',
+    0x25: 'noResponseFromSub-netComponent',
+    0x26: 'failurePreventsExecutionOfRequestedAction',
     0x31: 'requestOutOfRange',
     0x33: 'securityAccessDenied',
+    0x35: 'invalidKey',
+    0x36: 'exceededNumberOfAttempts',
+    0x37: 'requiredTimeDelayNotExpired',
+    0x70: 'uploadDownloadNotAccepted',
+    0x71: 'transferDataSuspended',
     0x72: 'generalProgrammingFailure',
+    0x73: 'wrongBlockSequenceCounter',
     0x78: 'requestCorrectlyReceivedResponsePending',
+    0x7E: 'sub-FunctionNotSupportedInActiveSession',
     0x7F: 'serviceNotSupportedInActiveSession'
 }
 

ed791755a42aa023bbb93f28721aa7867c9f5308
Kasper Karlsson <kasper.karlsson@omegapoint.se>
2017-08-18 13:52:11 +0200

    Updated module template - code, comments and usage

--- tool/template/module_template.py
+++ tool/template/module_template.py
@@ -1,18 +1,35 @@
-from can_actions import CanActions
+# module_template.py
+#
+# This file contains a template for a simple CaringCaribou module.
+# The module's entry point is the 'module_main' function.
+#
+# Steps to add this module to CaringCaribou and run it:
+#
+# 1. Copy this file to caringcaribou/tool/modules/
+#      $ cp module_template.py ../modules/
+#
+# 2. Go to caringcaribou/tool
+#      $ cd ..
+#
+# 3. Run the following command to run module and show usage instructions:
+#      $ ./cc.py module_template -h
+#
+
+from can_actions import CanActions, int_from_str_base
 from time import sleep
 import argparse
 
 
 def foo(arb_id):
     """
-    Stub function that sends a single message on the can bus and prints incoming traffic for 3 seconds
+    Performs some example operations, such as sending and receiving CAN messages.
 
-    :param arb_id: The arbitration id to send a message on
+    :param arb_id: The arbitration id to use when sending message 1
     """
 
     # Define a callback function which will handle incoming messages
     def example_response_handler(msg):
-        print("Incoming message!")
+        print("Callback handler: Incoming message!")
         print(msg)
         # Examples of how to filter data
         if msg.data[0] > 0x0F:
@@ -23,16 +40,35 @@ def foo(arb_id):
 
     print("Setting up")
     # Create an instance of CanActions, which we can use to send and receive messages.
-    # In this example we set the arbitration ID for outgoing messages directly in the constructor.
     with CanActions(arb_id) as can_wrap:
-        print("Sending message")
-        # Define some message contents
-        message = [0x11, 0x22, 0x33, 0x44]
-        # Send the message on the CAN bus and register a callback handler for incoming messages
-        can_wrap.send_single_message_with_callback(message, example_response_handler)
+        # EXAMPLE 1 - SEND MESSAGE WITH CALLBACK
+        # Define message contents
+        message1 = [0x11, 0x22, 0x33, 0x44]
+        # Number of seconds for callback handler to be active
+        callback_handler_duration = 3
+        print("Sending message 1 and adding callback function")
+        # Send the message on the CAN bus (using default arbitration ID) and register a callback
+        # handler for incoming messages
+        can_wrap.send_single_message_with_callback(message1, example_response_handler)
+        print("Letting callback handler be active for {0} seconds".format(callback_handler_duration))
         # Wait for three seconds before closing the CanActions instance. This means we have three
         # seconds to handle incoming messages through the callback function.
-        sleep(3)
+        sleep(callback_handler_duration)
+        # Manually remove the callback function. This is only needed since we want to proceed
+        # without keeping the callback handler in the next example - otherwise it would be
+        # automatically removed once can_wrap is closed.
+        can_wrap.clear_listeners()
+        print("Removed callback handler")
+
+        # EXAMPLE 2 - SEND MESSAGE TO CUSTOM ARBITRATION ID
+        # Define message contents
+        message2 = [0x55, 0x66, 0x77, 0x88, 0x99, 0xAA]
+        # Define custom arbitration ID
+        my_arb_id = 0x123
+        print("Sending message 2")
+        # Send message on the CAN bus using the custom arbitration ID
+        can_wrap.send(message2, my_arb_id)
+    # When we reach here, can_wrap has been closed
     print("Done!")
 
 
@@ -48,21 +84,27 @@ def parse_args(args):
                                      formatter_class=argparse.RawDescriptionHelpFormatter,
                                      description="Descriptive message for the template module",
                                      epilog="""Example usage:
-  cc.py module_template -arbId 1""")
+  cc.py module_template -arbId 123
+  cc.py module_template -arbId 0x1FF""")
 
-    parser.add_argument("-arbId", type=int, default=0)
+    parser.add_argument("-arbId", type=str, default="0", help="arbitration ID to use")
 
     args = parser.parse_args(args)
     return args
 
+
 def module_main(arg_list):
     """
-    Module main wrapper.
+    Module main wrapper. This is the entry point of the module when called by cc.py
 
-    :param arg_list: Module argument list passed from cc.py
+    :param arg_list: Module argument list passed by cc.py
     """
     try:
+        # Parse arguments
         args = parse_args(arg_list)
-        foo(args.arbId)
+        # Parse arbitration ID from the arguments (this function resolves both base 10 and hex values)
+        arbitration_id = int_from_str_base(args.arbId)
+        # Time to actually do stuff
+        foo(arbitration_id)
     except KeyboardInterrupt:
         print("\n\nTerminated by user")

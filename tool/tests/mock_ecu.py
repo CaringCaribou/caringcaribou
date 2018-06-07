a9ec7ddbb3e239d65f6f862c3e0f3f52ff2996fd
Kasper Karlsson <kasper.karlsson@omegapoint.se>
2018-06-07 15:03:15 +0200

    Base of single frame (SF) test for ISO-TP

--- tool/tests/mock_ecu.py
+++ tool/tests/mock_ecu.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 import can
+import datetime
 import time
 
 
@@ -7,6 +8,7 @@ class MockEcu:
     """Mock ECU base class, used for running tests over a virtual CAN bus"""
 
     virtual_test_bus = can.interface.Bus("test", bustype="virtual")
+    DELAY_BEFORE_RESPONSE = 0.05
 
     def __init__(self, bus=virtual_test_bus):
         self.bus = bus
@@ -28,6 +30,36 @@ class MockEcu:
         time.sleep(0.1)
 
 
+class MockEcuIsoTp(MockEcu):
+    """ISO-15765-2 (ISO-TP) mock ECU handler"""
+
+    MOCK_SINGLE_FRAME_REQUEST = [0x07, 0x01, 0x02, 0x03, 0x04, 0xAA, 0xAB]
+    MOCK_SINGLE_FRAME_RESPONSE = [0x01, 0x11, 0x22, 0x33, 0x44]
+
+    def __init__(self, arb_id_request, arb_id_response, bus=MockEcu.virtual_test_bus):
+        MockEcu.__init__(self, bus)
+        self.ARBITRATION_ID_REQUEST = arb_id_request
+        self.ARBITRATION_ID_RESPONSE = arb_id_response
+
+    def message_handler(self, message):
+        """
+        Logic for responding to incoming messages.
+
+        :param message: Incoming can.Message
+        :return: None
+        """
+        assert isinstance(message, can.Message)
+        if message.arbitration_id == self.ARBITRATION_ID_REQUEST:
+            # Simulate a small delay before responding
+            time.sleep(self.DELAY_BEFORE_RESPONSE)
+            if list(message.data)[1:] == self.MOCK_SINGLE_FRAME_REQUEST:
+                msg = can.Message(arbitration_id=self.ARBITRATION_ID_RESPONSE, data=self.MOCK_SINGLE_FRAME_RESPONSE)
+                self.bus.send(msg)
+            else:
+                # TODO Add more cases here
+                print("Unmapped")
+
+
 class MockEcuIso14229(MockEcu):
     """ISO-14229-1 (Unified Diagnostic Services) mock ECU handler"""
 
@@ -37,6 +69,12 @@ class MockEcuIso14229(MockEcu):
         self.ARBITRATION_ID_ISO_14229_RESPONSE = arb_id_response
 
     def message_handler(self, message):
+        """
+        Logic for responding to incoming messages.
+
+        :param message: Incoming can.Message
+        :return: None
+        """
         assert isinstance(message, can.Message)
         # TODO Implement logic to actually respond to requests
         if message.arbitration_id == self.ARBITRATION_ID_ISO_14229_REQUEST:
--- tool/tests/test_iso_15765_2.py
+++ tool/tests/test_iso_15765_2.py
@@ -1,17 +1,39 @@
 from __future__ import print_function
+from .mock_ecu import MockEcuIsoTp
 from lib import iso15765_2
 import can
+import datetime  # TODO remove
 import unittest
 
 
 class IsoTpTestCase(unittest.TestCase):
 
-    ARB_ID_REQUEST = 0x2000
-    ARB_ID_RESPONSE = 0x4000
+    ARB_ID_REQUEST = 0x100A
+    ARB_ID_RESPONSE = 0x100B
 
-    def test_create_iso_tp(self):
-        bus = can.interface.Bus("test", bustype="virtual")
-        with iso15765_2.IsoTp(self.ARB_ID_REQUEST, self.ARB_ID_RESPONSE, bus=bus) as tp:
-            self.assertTrue(isinstance(tp, iso15765_2.IsoTp), "Failed to initialize ISO-15765-2")
+    TIMEOUT_SECONDS = 3
 
-    # TODO Write tests
+    def setUp(self):
+        # Initialize virtual CAN bus
+        can_bus = can.interface.Bus("test", bustype="virtual")
+        # Initialize mock ECU
+        self.ecu = MockEcuIsoTp(self.ARB_ID_REQUEST, self.ARB_ID_RESPONSE, bus=can_bus)
+        self.ecu.add_listener(self.ecu.message_handler)
+        # Setup ISO-TP layer
+        self.tp = iso15765_2.IsoTp(self.ARB_ID_REQUEST, self.ARB_ID_RESPONSE, bus=can_bus)
+
+    def tearDown(self):
+        if isinstance(self.tp, iso15765_2.IsoTp):
+            self.tp.__exit__(None, None, None)
+
+    def test_create_iso_15765_2(self):
+        self.assertIsInstance(self.tp, iso15765_2.IsoTp, "Failed to initialize ISO-TP")
+
+    def test_single_frame(self):
+        # Send request
+        self.tp.request(MockEcuIsoTp.MOCK_SINGLE_FRAME_REQUEST)
+        # Receive response
+        response = self.tp.indication(self.TIMEOUT_SECONDS)
+        # Validate response
+        self.assertIsNotNone(response, "No SF response received")
+        self.assertEqual(list(response), MockEcuIsoTp.MOCK_SINGLE_FRAME_RESPONSE[1:])

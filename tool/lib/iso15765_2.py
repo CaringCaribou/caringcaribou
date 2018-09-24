16d18b86e829121f61fd6acb1d893ff7fc3a4d39
Kasper Karlsson <kasper.karlsson@omegapoint.se>
2018-09-24 11:49:09 +0200

    Added indication support for both keeping padding and simulating overflow behavior

--- tool/lib/iso15765_2.py
+++ tool/lib/iso15765_2.py
@@ -21,6 +21,7 @@ class IsoTp:
 
     FC_FS_CTS = 0
     FC_FS_WAIT = 1
+    FC_FS_OVFLW = 2
 
     SF_FRAME_ID = 0
     FF_FRAME_ID = 1
@@ -165,11 +166,13 @@ class IsoTp:
         frames = self.get_frames_from_message(message)
         self.transmit(frames, self.arb_id_response, self.arb_id_request)
 
-    def indication(self, wait_window=None):
+    def indication(self, wait_window=None, trim_padding=True, first_frame_only=False):
         """
         Receives an ISO-15765-2 message (one or more frames) and returns its content.
 
         :param wait_window: Max time (in seconds) to wait before timeout
+        :param trim_padding: If True, removes message padding bytes from the received message
+        :param first_frame_only: If True, return first frame only (simulating overflow behavior for multi-frame message)
         :return: A list of received data bytes if successful, None otherwise
         """
         message = []
@@ -181,7 +184,7 @@ class IsoTp:
         message_length = 0
 
         while True:
-            msg = self.bus.recv(self.N_BS_TIMEOUT)
+            msg = self.bus.recv(wait_window)
             if msg is not None:
                 if msg.arbitration_id == self.arb_id_request:
                     flow_control_arbitration_id = self.arb_id_response
@@ -196,12 +199,21 @@ class IsoTp:
                     if frame_type == self.SF_FRAME_ID:
                         # Single frame (SF)
                         dl, message = self.decode_sf(frame)
-                        # Trim padding if length exceeds single frame data length (SF_DL)
-                        message = message[:dl]
+                        if trim_padding:
+                            # Trim padding, in case the data exceeds single frame data length (SF_DL)
+                            message = message[:dl]
                         break
                     elif frame_type == self.FF_FRAME_ID:
                         # First frame (FF) of a multi-frame message
                         message_length, message = self.decode_ff(frame)
+                        if first_frame_only:
+                            # This is a hack to make it possible to only retrieve the first frame of a multi-frame
+                            # response, by telling the sender to stop sending data due to overflow
+                            ovflw_frame = self.encode_fc(self.FC_FS_OVFLW, 0, 0)
+                            # Respond with overflow (OVFLW) message
+                            self.send_message(ovflw_frame, flow_control_arbitration_id)
+                            # Return the first frame only
+                            break
                         fc_frame = self.encode_fc(self.FC_FS_CTS, 0, 0)
                         sn = 0
                         # Respond with flow control (FC) message
@@ -212,11 +224,12 @@ class IsoTp:
                         if (sn + 1) % 16 == new_sn:
                             sn = new_sn
                             message += data
-                            if len(message) == message_length:
-                                break
-                            elif len(message) > message_length:
-                                # Trim padding if message length exceeds first frame data length (FF_DL)
-                                message = message[:message_length]
+                            if len(message) >= message_length:
+                                # Last frame received
+                                if trim_padding:
+                                    # Trim padding of last frame, which may exceed first frame data length (FF_DL)
+                                    message = message[:message_length]
+                                # Stop listening for more frames
                                 break
                             else:
                                 pass
@@ -279,10 +292,13 @@ class IsoTp:
 
                         if number_of_frames_left_to_send < number_of_frames_left_to_send_in_block or block_size == 0:
                             number_of_frames_left_to_send_in_block = number_of_frames_left_to_send
-                        # If STmin is specified in microseconds (0x80-0xF0) or using reserved ranges (0x80-0xF0 and
+                        # If STmin is specified in microseconds (0xF1-0xF9) or using reserved ranges (0x80-0xF0 and
                         # 0xFA-0xFF), round up to one millisecond
                         if st_min > 0x7F:
                             st_min = 1
+                    elif fs == self.FC_FS_OVFLW:
+                        # Overflow - abort transmission
+                        return None
                     else:
                         # Timeout - did not receive a CTS message in time
                         return None

59a9bee4e1d07664317ec472f4c6fc3cafddc6dc
Kasper Karlsson <kasper.karlsson@omegapoint.se>
2018-10-29 15:32:04 +0100

    Iterative message handling to decrease CPU load. Also added message separator argument.

--- tool/modules/dump.py
+++ tool/modules/dump.py
@@ -1,34 +1,47 @@
 from __future__ import print_function
-from lib.can_actions import CanActions, int_from_str_base, msg_to_candump_format
+from lib.can_actions import DEFAULT_INTERFACE, int_from_str_base, msg_to_candump_format
 from modules.send import FILE_LINE_COMMENT_PREFIX
 from sys import argv, stdout
 import argparse
+import can
 import datetime
 
 
-def initiate_dump(handler, args):
+def initiate_dump(handler, whitelist, separator_seconds, candump_format):
     """
-    Adds a CAN message handler which should add found data to found_arb_ids.
-    Prints all of these afterwards, sorted by the number of hits.
+    Runs the 'handler' function on all incoming CAN messages.
 
-    :param handler: Message handler function
-    :param args: Argument namespace (reversed sorting applied if args.reverse)
+    Filtering is controlled by the list 'args.whitelist'
+    A separator is printed between messages if no messages have been handled in float 'args.separator_seconds'
+
+    :param handler: function to call on all incoming messages
+    :param whitelist: list of allowed arbitration IDs, or None to allow all
+    :param separator_seconds: float seconds before printing a separator between messages, or None to never do this
+    :param candump_format: bool indicating whether messages should be passed to 'handler' in candump str format
     """
-    whitelist = [int_from_str_base(x) for x in args.whitelist]
-    if args.candump_format:
+
+    if candump_format:
         format_func = msg_to_candump_format
     else:
         format_func = str
-
-    def whitelist_handling(msg):
-        if len(whitelist) == 0 or msg.arbitration_id in whitelist:
-            handler(format_func(msg))
+    separator_enabled = separator_seconds is not None
+    last_message_timestamp = datetime.datetime.min
+    last_message_separator_printed = True
 
     print("Dumping CAN traffic (press Ctrl+C to exit)".format(whitelist))
-    with CanActions() as can_wrap:
-        can_wrap.add_listener(whitelist_handling)
-        while True:
-            pass
+    with can.Bus(DEFAULT_INTERFACE, "socketcan") as bus:
+        for msg in bus:
+            # Separator handling
+            if separator_enabled and not last_message_separator_printed:
+                if (datetime.datetime.now() - last_message_timestamp).total_seconds() > separator_seconds:
+                    # Print separator
+                    handler("---")
+                    last_message_separator_printed = True
+            # Message handling
+            if len(whitelist) == 0 or msg.arbitration_id in whitelist:
+                handler(format_func(msg))
+                last_message_timestamp = datetime.datetime.now()
+                last_message_separator_printed = False
 
 
 def parse_args(args):
@@ -56,6 +69,11 @@ def parse_args(args):
                         action="store_true",
                         dest="candump_format",
                         help="Output on candump format")
+    parser.add_argument("-s",
+                        type=float,
+                        metavar="SEC",
+                        dest="separator_seconds",
+                        help="Print separating line after SEC silent seconds")
     args = parser.parse_args(args)
     return args
 
@@ -81,9 +99,13 @@ def module_main(args):
     :param args: List of module arguments
     """
     args = parse_args(args)
+    whitelist = [int_from_str_base(x) for x in args.whitelist]
+    separator_seconds = args.separator_seconds
+    candump_format = args.candump_format
+
     # Print to stdout
     if args.file is None:
-        initiate_dump(print, args)
+        initiate_dump(print, whitelist, separator_seconds, candump_format)
     # Print to file
     else:
         try:
@@ -102,6 +124,6 @@ def module_main(args):
                     output_file.write("{0}\n".format(line))
                     stdout.flush()
 
-                initiate_dump(write_line_to_file, args)
+                initiate_dump(write_line_to_file, whitelist, separator_seconds, candump_format)
         except IOError as e:
             print("IOError: {0}".format(e))

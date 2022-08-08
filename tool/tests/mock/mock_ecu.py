5047803d5d92e3f937a3192d8cea7f488ef52f83
Tomer Fichman <Tomer.Fichman@cymotive.com>
2022-08-08 11:51:44 +0300

    forgot to remove some experimental stuff

--- tool/tests/mock/mock_ecu.py
+++ tool/tests/mock/mock_ecu.py
@@ -1,5 +1,5 @@
 from __future__ import print_function
-from lib.can_actions import DEFAULT_INTERFACE, pcan, global_bus
+from lib.can_actions import DEFAULT_INTERFACE, pcan
 import can
 from can.interfaces.pcan import PcanBus
 
@@ -10,7 +10,13 @@ class MockEcu:
     DELAY_BEFORE_RESPONSE = 0.01
 
     def __init__(self, bus=None):
-        self.bus = global_bus
+        self.message_process = None
+        if pcan:
+            self.bus = PcanBus()
+        elif bus is None:
+            self.bus = can.Bus(DEFAULT_INTERFACE)
+        else:
+            self.bus = bus
 
     def __enter__(self):
         return self

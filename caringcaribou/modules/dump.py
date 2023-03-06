05d7febf236b831b44f314149f7dc9ff4156571c
Alexander Alasjö <alexander@alasjo.se>
2023-03-06 17:36:49 +0100

    Fix imports

--- caringcaribou/modules/dump.py
+++ caringcaribou/modules/dump.py
@@ -1,7 +1,7 @@
 from __future__ import print_function
 from caringcaribou.utils.can_actions import CanActions
 from caringcaribou.utils.common import msg_to_candump_format, parse_int_dec_or_hex
-from modules.send import FILE_LINE_COMMENT_PREFIX
+from caringcaribou.modules.send import FILE_LINE_COMMENT_PREFIX
 from sys import argv, stdout
 import argparse
 import datetime
